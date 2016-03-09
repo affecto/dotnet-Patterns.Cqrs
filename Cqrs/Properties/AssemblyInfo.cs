1ddf039fcdf88776ee7c2fb6ce9d706c80e72523
johannesvaltonen <johannes.valtonen@affecto.com>
2016-03-09 14:47:28 +0200

    Release version 3.0.0.

--- Cqrs/Properties/AssemblyInfo.cs
+++ Cqrs/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@
 [assembly: AssemblyFileVersion("3.0.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("3.0.0-prerelease01")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
